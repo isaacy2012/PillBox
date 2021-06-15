75258e8d6dae6830152a7ac67eba2466aa31f50a
isaacy2012 <50868057+isaacy2012@users.noreply.github.com>
2021-06-15 16:15:27 +1200

    Added more tests for Assertions

--- pillBox/app/src/test/java/com/innerCat/pillBox/AssertionTests.java
+++ pillBox/app/src/test/java/com/innerCat/pillBox/AssertionTests.java
@@ -10,10 +10,11 @@ public class AssertionTests {
     public void assert_true_false_no_message() {
         try {
             Assertions.assertTrue(false);
-            assert false;
         } catch (AssertionError e) {
             assertEquals(e.getMessage(), "Assertion Failed");
+            return;
         }
+        assert false;
     }
 
     @Test
@@ -25,19 +26,104 @@ public class AssertionTests {
     public void assert_true_false_with_message() {
         try {
             Assertions.assertTrue(false, "Message");
-            assert false;
         } catch (AssertionError e) {
             assertEquals(e.getMessage(), "Message");
+            return;
         }
+        assert false;
     }
 
     @Test
     public void assert_true_false_with_multiple_message() {
         try {
             Assertions.assertTrue(false, "Message", "Message2");
-            assert false;
         } catch (AssertionError e) {
             assertEquals("Message\nMessage2", e.getMessage());
+            return;
         }
+        assert false;
+    }
+
+    @Test
+    public void assert_false_true_no_message() {
+        try {
+            Assertions.assertFalse(true);
+        } catch (AssertionError e) {
+            assertEquals(e.getMessage(), "Assertion Failed");
+            return;
+        }
+        assert false;
+    }
+
+    @Test
+    public void assert_false_false_no_message() {
+        Assertions.assertFalse(false);
+    }
+
+    @Test
+    public void assert_false_true_with_message() {
+        try {
+            Assertions.assertFalse(true, "Message");
+        } catch (AssertionError e) {
+            assertEquals(e.getMessage(), "Message");
+            return;
+        }
+        assert false;
+    }
+
+    @Test
+    public void assert_false_true_with_multiple_message() {
+        try {
+            Assertions.assertFalse(true, "Message", "Message2");
+        } catch (AssertionError e) {
+            assertEquals("Message\nMessage2", e.getMessage());
+            return;
+        }
+        assert false;
+    }
+
+    @Test
+    public void assert_null_null() {
+        Assertions.assertNull(null);
+    }
+
+    @Test
+    public void assert_null_notnull() {
+        try {
+            Assertions.assertNull(2);
+        } catch (AssertionError ignored) {
+            return;
+        }
+        assert false;
+    }
+
+    @Test
+    public void assert_notnull_notnull() {
+        Assertions.assertNotNull(2);
+    }
+
+    @Test
+    public void assert_notnull_null() {
+        try {
+            Assertions.assertNotNull(null);
+        } catch (AssertionError ignored) {
+            return;
+        }
+        assert false;
+    }
+
+    @Test
+    public void assert_equals_equals() {
+        Assertions.assertEquals(1, 1);
+    }
+
+    @Test
+    public void assert_equals_notequals() {
+        try {
+            Assertions.assertEquals(1, 2);
+        } catch (AssertionError ignored) {
+            return;
+        }
+        assert false;
     }
 }
