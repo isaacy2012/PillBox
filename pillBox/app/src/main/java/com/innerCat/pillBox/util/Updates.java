2f5e00afe8fdd22727fd34f20ac61215c9d29ad1
isaacy2012 <50868057+isaacy2012@users.noreply.github.com>
2021-08-25 01:13:01 +1200

    Changed app bar height

--- pillBox/app/src/main/java/com/innerCat/pillBox/util/Updates.java
+++ pillBox/app/src/main/java/com/innerCat/pillBox/util/Updates.java
@@ -10,8 +10,8 @@ public class Updates {
     /**
      * Sets update.
      *
-     * @param updateString the update string
-     * @param seen         the seen
+     * @param context the context
+     * @param seen    the seen
      */
     private static void setUpdate(Context context, boolean seen) {
         SharedPreferences.Editor editor = SharedPreferencesFactory.getSP(context).edit();
@@ -22,7 +22,7 @@ public class Updates {
     /**
      * Set a particular update as seen
      *
-     * @param updateString the update string
+     * @param context the context
      */
     public static void setUpdateSeen(Context context) {
         setUpdate(context, true);
@@ -31,7 +31,7 @@ public class Updates {
     /**
      * Set a particular update as unseen
      *
-     * @param updateString the update string
+     * @param context the context
      */
     public static void setUpdateUnseen(Context context) {
         setUpdate(context, false);
@@ -43,13 +43,13 @@ public class Updates {
 
     public static String getUpdateBodyString() {
         return "" +
-                "<b>Improvements</b>" +
+                "<b>Tweaks</b>" +
                 "<br>" +
-                "- You can now edit thresholds for stock and expiry warnings" +
+                "- Tweaked the size of the toolbar, moving the title down" +
                 "<br>" +
                 "<br>" +
                 "<b>Bugfixes</b>" +
-                "<br>- Fixed a bug where the stock text would sometimes be bold but not red" +
+                "<br>- Fixed a bug where the OK button was disabled by default when editing an item" +
                 "<br>" +
                 "<br>If you have any bug reports or feature requests, feel free to leave a review or post an issue on our <a href=\"https://github.com/isaacy2012/Pillbox\">GitHub repository</a>." +
                 "<br>" +
